280e6a53638d75b392f506f9ab9539934c47397f
XingC <123fengmo@gmail.com>
2021-12-06 13:37:15 +0800

    Fix bat content

--- pack as exe.bat
+++ pack as exe.bat
@@ -1,11 +1,11 @@
 chcp 65001
 @echo off
-TITLE      XingCZip 一键打包工具
+TITLE     autoClick_XingC 一键打包工具
 color 3f
 mode con cols=90 lines=29
 echo.
 ECHO. =================================================
-echo. 		XingCZip 一键打包工具 byXingC
+echo. 		autoClick_XingC 一键打包工具 byXingC
 echo     --按任意键继续
 echo.
 ECHO. =================================================
@@ -14,7 +14,7 @@ CLS
 ECHO. =================================================
 echo.	耐心等待...
 pyinstaller -F --workpath workpath --distpath . -n autoClick_XingC autoClick.py
-del /f /s XingCZip.spec
+del /f /s autoClick_XingC.spec
 rd /s /q __pycache__
 rd /s /q lib\__pycache__
 rd /s /q workpath
